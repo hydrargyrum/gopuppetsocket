29e0df6f9894d2abc80640af098f2be58da20563
Hg <dev@indigo.re>
2022-04-09 22:30:23 +0200

    use "flag" for options

--- client.go
+++ client.go
@@ -1,9 +1,14 @@
 package main
 
+import "flag"
+import "fmt"
 import "log"
 import "time"
 import "os"
 import "net"
+
+var realAddress = flag.String("real", "", "ADDRESS:PORT of the real end server that should be reached")
+var puppetAddress = flag.String("puppet", "", "ADDRESS:PORT of the puppet server")
 
 func copyTo(from, to net.Conn) {
 	buf := make([]byte, 1024)
@@ -34,9 +39,26 @@ func copyTo(from, to net.Conn) {
 	}
 }
 
+func checkAddr(addr string, label string) {
+	if len(addr) == 0 {
+		fmt.Fprintf(flag.CommandLine.Output(), "%s address cannot be empty\n", label)
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	if _, err := net.ResolveTCPAddr("tcp", addr); err != nil {
+		log.Fatalf("could not resolve %s address: %s", label, err)
+	}
+}
+
 func main() {
+	flag.Parse()
+
+	checkAddr(*realAddress, "real server")
+	checkAddr(*puppetAddress, "puppet server")
+
 	for {
-		puppetServer, err := net.Dial("tcp", os.Args[1])
+		puppetServer, err := net.Dial("tcp", *puppetAddress)
 		if err != nil {
 			log.Printf("could not connect to puppet server: %s", err)
 			time.Sleep(time.Duration(1000 * 1000 * 1000))
@@ -44,7 +66,7 @@ func main() {
 		}
 		log.Printf("connected to puppet server")
 
-		realServer, err := net.Dial("tcp", os.Args[2])
+		realServer, err := net.Dial("tcp", *realAddress)
 		if err != nil {
 			log.Printf("could not connect to real server: %s", err)
 			puppetServer.Close()
--- server.go
+++ server.go
@@ -1,10 +1,16 @@
 package main
 
+import "flag"
+import "fmt"
 import "log"
 
 //import "time"
 import "os"
 import "net"
+import "strconv"
+
+var realAddress = flag.String("real", "", "ADDRESS:PORT on which to listen connections from a real client")
+var puppetAddress = flag.String("puppet", "", "ADDRESS:PORT on which to listen for the puppet client")
 
 func copyTo(from, to net.Conn) {
 	buf := make([]byte, 1024)
@@ -38,14 +44,14 @@ func copyTo(from, to net.Conn) {
 func handleRealClient(realClient net.Conn) {
 	defer realClient.Close()
 
-	puppet_listener, err := net.Listen("tcp", os.Args[2])
+	puppetListener, err := net.Listen("tcp", *puppetAddress)
 	if err != nil {
 		log.Printf("could not listen for puppet clients: %s", err)
 		return
 	}
 
-	puppetClient, err := puppet_listener.Accept()
-	puppet_listener.Close()
+	puppetClient, err := puppetListener.Accept()
+	puppetListener.Close()
 	if err != nil {
 		log.Printf("could not accept a puppet connection: %s", err)
 		return
@@ -56,8 +62,42 @@ func handleRealClient(realClient net.Conn) {
 	copyTo(realClient, puppetClient)
 }
 
+func checkAddr(addr string, label string) string {
+	if len(addr) == 0 {
+		fmt.Fprintf(flag.CommandLine.Output(), "%s address cannot be empty\n", label)
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	_, _, err := net.SplitHostPort(addr)
+	switch err := err.(type) {
+	case nil:
+	case *net.AddrError:
+		if err.Err == "missing port in address" {
+			_, err := strconv.Atoi(addr)
+			if err != nil {
+				log.Fatalf("%s address is not in ADDRESS:PORT or PORT format", label)
+			}
+			addr = fmt.Sprintf("0.0.0.0:%s", addr)
+		}
+	default:
+		log.Fatalf("could not parse %s address: %s", label, err)
+	}
+
+	if _, err := net.ResolveTCPAddr("tcp", addr); err != nil {
+		log.Fatalf("could not resolve %s address: %s", label, err)
+	}
+
+	return addr
+}
+
 func main() {
-	realListener, err := net.Listen("tcp", os.Args[1])
+	flag.Parse()
+
+	*realAddress = checkAddr(*realAddress, "real")
+	*puppetAddress = checkAddr(*puppetAddress, "puppet")
+
+	realListener, err := net.Listen("tcp", *realAddress)
 	if err != nil {
 		log.Fatalf("could not listen for real clients: %s", err)
 	}
@@ -72,5 +112,4 @@ func main() {
 
 		go handleRealClient(realClient)
 	}
-
 }
